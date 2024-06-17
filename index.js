4d09bae87a72a8821e82df400ceac2cae1874579
prietse <jhonpritse@gmail.com>
2024-06-17 22:50:24 +0800

    added index js and opening page

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-﻿//Test
+﻿var express = require("express");
+const WS_MODULE = require("ws");
+const http = require("https");
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get("/hello", (req, res) => {
+    res.send("hello world");
+});
+
+
+const server = http.createServer({
+   // key: fs.readFileSync('fm.thepocketportal.com_production_privkey.pem'),
+  //  cert: fs.readFileSync('fm.thepocketportal.com_production_cert.pem')
+},app);
+
+ws = new WS_MODULE.Server({server});
+
+server.listen(process.env.PORT, () => {
+    console.log("Authentication Server turned on, port number:" + process.env.PORT);
+});
