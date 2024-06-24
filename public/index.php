327599c311349df412a72bfe173efcb6595907c3
prietse <jhonpritse@gmail.com>
2024-06-24 23:56:12 +0800

    log in page 32

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 ﻿<?php
-require __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+//require __DIR__ . '/vendor/autoload.php';
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
 ?>
 
 <html lang="">
