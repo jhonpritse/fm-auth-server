2534d9f848589a9b628efdcdb4eff4d9bbb71df7
prietse <jhonpritse@gmail.com>
2024-06-27 00:16:57 +0800

    log in page 42

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 ﻿<?php
-//require __DIR__ . '/vendor/autoload.php';
-//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-//$dotenv->load();
-//
-//$config = parse_ini_file(__DIR__ . '/config.ini', true);
 require __DIR__ . '/config.php';
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
 
 
@@ -14,18 +14,6 @@ require __DIR__ . '/config.php';
     </head>
     <body>
         <h1>PHP MySQL Connection</h1>
-
-dsa
-        <?php
-      
-        // Create connection
-        // Check connection
-        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
-        ?>
         mini
     </body>
 </html>
