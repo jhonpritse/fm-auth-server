ed19a40ce81a57a50da235ad4312eec92911e854
prietse <jhonpritse@gmail.com>
2024-06-27 00:24:35 +0800

    log in page 42

--- public/index.php
+++ public/index.php
@@ -14,6 +14,17 @@ echo "Connected successfully";
     </head>
     <body>
         <h1>PHP MySQL Connection</h1>
-        mini
+<?php
+$result = mysqli_query($conn,
+    "SELECT * FROM `pocketportal-db`.user");
+$data = $result->fetch_all(MYSQLI_ASSOC);
+?>
+<?php foreach($data as $row): ?>
+    <tr>
+        <td><?= htmlspecialchars($row['user']) ?></td>
+        <td><?= htmlspecialchars($row['password']) ?></td>
+    </tr>
+<?php endforeach ?>
+        --end--
     </body>
 </html>
